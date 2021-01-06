596471eae62905dd2a2f61ce157de78519aa1fe0
Mateusz Dudziec <m.dudziec@agsystems.pl>
2021-01-06 10:53:20 +0100

    change url

--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -1,25 +1,29 @@
 import axios from "axios";
 
 export const getCategories = async () =>
-  await axios.get(`http://localhost:5000/api/categories`);
+  await axios.get(`https://shop-md.herokuapp.com/api/categories`);
 
 export const getCategory = async (slug) =>
-  await axios.get(`http://localhost:5000/api/category/${slug}`);
+  await axios.get(`https://shop-md.herokuapp.com/api/category/${slug}`);
 
 export const removeCategory = async (slug, authtoken) =>
-  await axios.delete(`http://localhost:5000/api/category/${slug}`, {
+  await axios.delete(`https://shop-md.herokuapp.com/api/category/${slug}`, {
     headers: { "Access-Control-Allow-Origin": true, authtoken },
   });
 
 export const updateCategory = async (slug, category, authtoken) =>
-  await axios.put(`http://localhost:5000/api/category/${slug}`, category, {
-    headers: { "Access-Control-Allow-Origin": true, authtoken },
-  });
+  await axios.put(
+    `https://shop-md.herokuapp.com/api/category/${slug}`,
+    category,
+    {
+      headers: { "Access-Control-Allow-Origin": true, authtoken },
+    }
+  );
 
 export const createCategory = async (name, authtoken) =>
-  await axios.post(`http://localhost:5000/api/category`, name, {
+  await axios.post(`https://shop-md.herokuapp.com/api/category`, name, {
     headers: { "Access-Control-Allow-Origin": true, authtoken },
   });
 
 export const getCategorySubs = async (id) =>
-  await axios.get(`http://localhost:5000/api/category/subs/${id}`);
+  await axios.get(`https://shop-md.herokuapp.com/api/category/subs/${id}`);
